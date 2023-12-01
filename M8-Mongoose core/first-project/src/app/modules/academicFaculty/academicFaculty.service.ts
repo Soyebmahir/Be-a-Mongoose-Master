a5a96b8699ba135768d1a7fbba3a740285023431
Soyeb <tssoyeb@gmail.com>
2023-12-01 15:45:11 +0600

    academicFaculttyServices created

---
+++ M8-Mongoose core/first-project/src/app/modules/academicFaculty/academicFaculty.service.ts
@@ -0,0 +1,29 @@
+import { TAcademicFaculty } from "./academicFaculty.interface";
+import { AcademicFaculty } from "./academicFaculty.model";
+
+const createAcademicFacultyIntoDB = async (payload: TAcademicFaculty) => {
+    const result = await AcademicFaculty.create(payload);
+    return result;
+}
+
+const getAllAcademicFaculty = async () => {
+    const result = await AcademicFaculty.find({});
+    return result;
+}
+const getSingleAcademicFacultyFromDb = async (id: string) => {
+    const result = await AcademicFaculty.findById(id);
+    return result
+}
+
+const updateFacultyIntoDB = async (id: string, payload: Partial<TAcademicFaculty>) => {
+    const result = await AcademicFaculty.findByIdAndUpdate({ _id: id }, payload)
+    return result;
+
+}
+
+export const AcademicFacultyServices = {
+    createAcademicFacultyIntoDB,
+    getAllAcademicFaculty,
+    getSingleAcademicFacultyFromDb,
+    updateFacultyIntoDB
+}
