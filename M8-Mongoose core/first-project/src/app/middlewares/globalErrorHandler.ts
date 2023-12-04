cb3c3f066a6187dcc634a12f0ff6e693c4649538
Soyeb <tssoyeb@gmail.com>
2023-12-04 22:02:58 +0600

    same pattern for all kind of error

--- M8-Mongoose core/first-project/src/app/middlewares/globalErrorHandler.ts
+++ M8-Mongoose core/first-project/src/app/middlewares/globalErrorHandler.ts
@@ -2,19 +2,59 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { NextFunction, Request, Response } from 'express';
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import config from '../config';
+const globalErrorHandler: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next,
 ) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'something Went wrong';
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'something Went wrong';
+  type TErrorSources = {
+    path: string | number,
+    message: string
+  }[]
+  let errorSources: TErrorSources = [{
+    path: "",
+    message: 'Something went Wrong'
+
+  }]
+
+  //zod error handler
+  const handleZodError = (err: ZodError) => {
+    const errorSources = err.issues.map((issue) => {
+      return {
+        path: issue.path[issue.path.length - 1],
+        message: issue.message
+
+      }
+    })
+    statusCode = 400;
+    message = 'validation Error'
+    return {
+      statusCode,
+      message,
+      errorSources
+
+    }
+  }
+  //error checking
+  if (err instanceof ZodError) {
+    const modifiedError = handleZodError(err)
+    statusCode = modifiedError.statusCode;
+    message = modifiedError.message;
+    errorSources = modifiedError.errorSources
+  }
+
+
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? err.stack : null
   });
 };
 export default globalErrorHandler;
