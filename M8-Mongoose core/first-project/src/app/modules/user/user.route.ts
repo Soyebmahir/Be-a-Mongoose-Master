1ef3ebf247866a9dd6726c3584fec07519514df5
Soyeb <tssoyeb@gmail.com>
2023-12-25 11:47:42 +0600

    admin authorization on create student faculties

--- M8-Mongoose core/first-project/src/app/modules/user/user.route.ts
+++ M8-Mongoose core/first-project/src/app/modules/user/user.route.ts
@@ -5,21 +5,24 @@ import { studentValidations } from '../students/student.validation';
 import validateRequest from '../../middlewares/validaterequest';
 import { createFacultyValidationSchema } from '../faculty/faculty.validation';
 import { createAdminValidationSchema } from '../admin/admin.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './user.const';
 const router = express.Router();
 
 router.post(
-  '/create-student',
+  '/create-student', auth(USER_ROLE.admin),
   validateRequest(studentValidations.studentValidationSchema),
   UserControllers.createStudent,
 );
 router.post(
   '/create-faculty',
+  auth(USER_ROLE.admin),
   validateRequest(createFacultyValidationSchema),
   UserControllers.createFaculty,
 );
 
 router.post(
-  '/create-admin',
+  '/create-admin', auth(USER_ROLE.admin),
   validateRequest(createAdminValidationSchema),
   UserControllers.createAdmin,
 );
